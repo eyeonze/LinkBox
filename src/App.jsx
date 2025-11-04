e435d4917b4ac6f271a1cebbba5550b244debf50
eyeonze <leeyj1007987@g.skku.edu>
2025-11-04 23:01:07 +0900

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "16px" }}>
+      <h1>안녕하세요, React!</h1>
+      <p>이것은 React로 만든 확장 프로그램입니다.</p>
+    </div>
+  );
+}
+
+export default App;
